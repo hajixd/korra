811b8888fea5bed39ba792be4b7bc50cbc335654
haji <129047774+hajixd@users.noreply.github.com>
2026-03-22 17:25:14 -0700

    fds

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  width: 1280,
+  width: "device-width",
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
